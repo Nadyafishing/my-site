54f47249c147b822dd9149ce6b07e9ab512e0f98
NadyaFishing <saprykinanv@gmail.com>
2026-05-27 19:39:47 +0300

    beautiful blog layout4

--- my-site/components/Hero.tsx
+++ my-site/components/Hero.tsx
@@ -4,7 +4,8 @@ export default function Hero() {
       className="relative h-[500px] bg-cover bg-center flex items-center justify-center"
       style={{
         backgroundImage:
-          "url(https://images.unsplash.com/photo-1507525428034-b723cf961d3e?q=80&w=1600&auto=format&fit=crop)",
+          "url('/hero.jpg')",
+          /*"url(https://images.unsplash.com/photo-1507525428034-b723cf961d3e?q=80&w=1600&auto=format&fit=crop)",*/
       }}
     >
       <div className="absolute inset-0 bg-black/50" />
