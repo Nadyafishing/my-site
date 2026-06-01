cf521f56cd5958ffe17d8214141ff9d4195d4e05
NadyaFishing <saprykinanv@gmail.com>
2026-06-01 22:50:37 +0300

    Add pictures3

--- my-site/components/PostCard.tsx
+++ my-site/components/PostCard.tsx
@@ -11,7 +11,9 @@ type Props = {
 
 export default function PostCard({ slug, title, date, excerpt, image }: Props) {
   return (
-    <article className="bg-white rounded-2xl shadow-md p-6 hover:shadow-xl transition">
+    <Link href={`/posts/${slug}`} className="block">
+
+    <article className="bg-white rounded-2xl shadow-md p-6 hover:shadow-xl hover:-translate-y-1 transition cursor-pointer">
     {image && (
       <Image
         src={image}
@@ -21,17 +23,20 @@ export default function PostCard({ slug, title, date, excerpt, image }: Props) {
         className="w-full h-56 object-cover rounded-xl mb-4"
       />
     )}
-      <Link href={`/posts/${slug}`}>
         <h2 className="text-2xl font-bold mb-2 hover:text-blue-600 transition">
           {title}
         </h2>
-      </Link>
 
       <p className="text-gray-500 text-sm mb-4">{date}</p>
 
       <p className="text-gray-700 leading-relaxed">
         {excerpt}
       </p>
+      <p className="inline-block mt-4 px-4 py-2 rounded-full bg-gray-100">
+        Читать →
+      </p>
     </article>
+    
+    	</Link>
   );
 }
